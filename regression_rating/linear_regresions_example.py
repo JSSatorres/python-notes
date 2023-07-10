aa1554d2ec842b516ce9f8ff06957b09a469914c
jssatorres <juansataz.dev@gmail.com>
2023-07-10 17:35:49 +0200

    feat: linear regresion

--- regression_rating/linear_regresions_example.py
+++ regression_rating/linear_regresions_example.py
@@ -1,3 +1,53 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+
+# Generación del conjunto de datos
+
+X = 2 * np.random.rand(100, 1)
+y = 4 + 3 * X + np.random.randn(100, 1)
+
+print("La longitud del conjunto de datos es:", len(X))
+
+#  Visualización del conjunto de datos
+
+data = {'n_equipos_afectados': X.flatten(), 'coste': y.flatten()}
+df = pd.DataFrame(data)
+df.head(10)
+
+# Escalado del número de equipos afectados
+df['n_equipos_afectados'] = df['n_equipos_afectados'] * 1000
+df['n_equipos_afectados'] = df['n_equipos_afectados'].astype('int')
+
+# Escalado del coste
+df['coste'] = df['coste'] * 10000
+df['coste'] = df['coste'].astype('int')
+df.head(10)
+
+plt.plot(df['n_equipos_afectados'], df['coste'], "b.")
+plt.xlabel("Equipos afectados")
+plt.ylabel("Coste del incidente")
+plt.show()
+
+# Construcción del modelo y ajuste de la función hipótesis
+lin_reg = LinearRegression()
+# para entrenar la funcion fit(valores de entrada, valores de salida)
+lin_reg.fit(df['n_equipos_afectados'].values.reshape(-1, 1), df['coste'].values)
+
+# Parámetro theta 0
+lin_reg.intercept_
+
+# Parámetro theta 1
+lin_reg.coef_
+
+# Predicción para el valor mínimo y máximo del conjunto de datos de entrenamiento
+X_min_max = np.array([[df["n_equipos_afectados"].min()], [df["n_equipos_afectados"].max()]])
+y_train_pred = lin_reg.predict(X_min_max)
+
+# Representación gráfica de la función hipótesis generada
+plt.plot(X_min_max, y_train_pred, "g-")
+plt.plot(df['n_equipos_afectados'], df['coste'], "b.")
+plt.xlabel("Equipos afectados")
+plt.ylabel("Coste del incidente")
+plt.show()
