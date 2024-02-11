f401eb5edb3b5dbad81753997da22a224f10724a
JSSatorres <juansataz.dev@gmail.com>
2024-02-11 15:52:03 +0100

    fix: get frame add flask

--- phoneProject/main.py
+++ phoneProject/main.py
@@ -4,3 +4,8 @@ video = cv2.VideoCapture(1) # 0 for default camera, 1 for external camera
 
 def get_frame():
   success, frame = video.read()
+  sc, encoded_image =cv2.imencode('.jpg', frame)
+  frame = encoded_image.tobytes()
+  yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+app = Flask(__name__)
